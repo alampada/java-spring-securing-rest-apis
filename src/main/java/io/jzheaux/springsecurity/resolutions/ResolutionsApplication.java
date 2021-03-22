9c0e609a71eb93c08eca58f7d30ee4e0d40e7eb5
Antonios Lampadaridis <antonios.lampadaridis@gmail.com>
2021-03-22 22:18:49 +0000

    Part 4

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
@@ -19,9 +21,24 @@ public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
 				.authorizeRequests(authz -> authz
 						.anyRequest().authenticated())
 				.httpBasic(basic -> {})
-				.csrf().disable();
+				.csrf().disable()
+				.cors(cors -> {});
 	}
 
+
+	@Bean
+	WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.maxAge(0)
+						.allowedOrigins("http://localhost:4000")
+						.allowedMethods("HEAD")
+						.allowedHeaders("Authorization");
+			}
+		};
+	}
 	@Bean
 	public  UserDetailsService userDetailsService(UserRepository users) {
 		return new UserRepositoryUserDetailsService(users);
